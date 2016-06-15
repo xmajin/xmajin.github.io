dfc8674e465f79bf2483a1cdaae25514994d35d3
Tadas <tadas.skeberdis@gmail.com>
2016-06-15 17:00:10 +0300

    super update

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ function myName() {
    var name = prompt("Koks tava vardas?");
    document.getElementById("name").innerHTML = name;
    document.getElementById("tabName").innerHTML = name;
+   document.getElementById("button").innerHTML = "CHANGE NAME";
 }
 
+var click = 0;
 function headClick() {
-    document.getElementById("picHead").innerHTML = "Oy don't click my head, mate!";
-    document.getElementById("klikYes").innerHTML = "Yes";
+    click += 1;
+    document.getElementById("picHead").innerHTML = "Oy don't click my head, mate! "
+     + click + " times clicked.";
+    document.getElementById("klikYes").innerHTML = "Yes, " + "clicks: " + click;
 }
